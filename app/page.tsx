341993c904519d67189bd46786bdafcf5a2deba7
Azhar <eng.azharkhan@gmail.com>
2023-07-28 17:22:54 +0500

    popular events table

--- app/page.tsx
+++ app/page.tsx
@@ -17,11 +17,11 @@ const pop_events= [
   {
     id: 2,
     url: "link",
-    title: "England W vs Denmark W Live Stream",
+    title: "Guadalajara vs Cincinnati Live Streams",
     type: "soccer",
     time: "07:30 AM ET",
-    team_a: "England",
-    team_b: "Denmark"
+    team_a: "Guadalajara",
+    team_b: "Cincinnati"
   },
 
 ]
@@ -137,10 +137,11 @@ export default function Home() {
                       {
                           event.type == 'formula-1'
                           ? <a href="#" title={event.title} className="row MaclariListele align-items-center align-content-center">
-                            <Col xs={4}>
-                            {event.title}
+                            <Col xs={4} className='d-flex align-items-center'>
+                            <Image src="icon-f1.png" alt="formula-1-icon" className="responsive-img"/>
+                            <div style={{color : '#fff', fontWeight : 'bold', marginLeft : '5px'}}>{event.title}</div>
                             </Col>
-                            <Col xs={3}>
+                            <Col xs={3} className='league-logo'>
                               <Image src="icon-f1.png" alt="formula-1-icon" className="responsive-img"/>
                             </Col>
                             <Col xs={3}>
@@ -150,7 +151,28 @@ export default function Home() {
                             <Image src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAAB/ElEQVRIie2VsWsUQRjF37d3np3FIQiCR25nj7vS4mIhiIgWaUxxSWUEsRELFQRrBUX/ApsUgkKCkQSLNFqoIIKg3n9wO7tgERDCgYiI7N08m1sYbG5ml02VV833mO/9+GaGXeBQByRx2aS1/gDgwqz8mmXZpV6v96sMOHDcd8xan6nX6xskXXuLg4MgWAWwn9cisqy1flgG7HTUAJAkyTmS7wA0ZhYBrCmlXhYBOx9XGIafSN6zLAHwTGu9WATsPHGuJEnWSd7Ia5J7jUZjsdVq7fnkeD+Q8Xh8i+THvBaRk1mWbY9Go6OVgvv9fjadTlcBpJZ9tlarrVcKBoBut7sfBMEAwO/cI3ktjuPbrhned2xLaz0AsGPlTElejqLozbzeUh8BpdRrkk8sqyYiG3EcR5WCZ/D7JHctqykim5WDi6o0WGv9SESWLWtMcm1eX9nHtQJg28qZAFhSSr2f11t44jRNTwN4YUEhInddoEDBiZMkOUHyG4BTlv1cKXXdNcN74uFweMQY8+o/6GdjzE2fHG9ws9l8KiLnLeu7iAw6nc7fysBxHN+x/0wA/gBYCcPwh08O4HHHWuuLAN4CqM8skrwSRdGWLxRwnDhN0wUAWxYUIvK4KNQZbIzZAXA8r0nuttvtB0WhzmAAP631l8lkclVETBnwoQ5M/wBTeLAZ7QRNBwAAAABJRU5ErkJggg==" alt="watch" className="responsive-img"/>
                             </Col>
                           </a>
-                          : 'Hello, world!'
+                          :  <a href="#" title={event.title} className="row MaclariListele align-items-center align-content-center">
+                              <Col xs={4}>
+                                <div className='d-flex align-items-center'>
+                                  <Image src="427.png" alt="Guadalajara" className="responsive-img"/>
+                                  <div style={{color : '#fff', fontWeight : 'bold', marginLeft : '5px'}}>{event.team_a}</div>
+                                </div>
+                                <br/>
+                                <div className='d-flex align-items-center'>
+                                  <Image src="3636.png" alt="formula-1-icon" className="responsive-img"/>
+                                  <div style={{color : '#fff', fontWeight : 'bold', marginLeft : '5px'}}>{event.team_b}</div>
+                                </div>
+                            </Col>
+                            <Col xs={3} className='league-logo'>
+                              <Image src="league_placeholder.png" alt="league cup" className="responsive-img"/>
+                            </Col>
+                            <Col xs={3}>
+                            <span className="live-btn">LIVE</span>
+                            </Col>
+                            <Col xs={1}>
+                            <div className="btn btn-success btn-sm MobildeGizle">Watch</div>
+                            </Col>
+                          </a>
                       }
                   </div>
                 ))
