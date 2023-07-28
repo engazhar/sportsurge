5b8a7532b3fda0020e0b00d74a53ca830d4dab63
Azhar <eng.azharkhan@gmail.com>
2023-07-28 18:23:46 +0500

    Blog section added Home

--- app/page.tsx
+++ app/page.tsx
@@ -26,6 +26,28 @@ const pop_events= [
 
 ]
 
+const sport_blogs =[
+  {
+    url: "#",
+    title: '10 Top Female MMA Fighters in 2023',
+    excerpt: "Mixed Martial Arts (MMA) has gone a long way since its start, and female competitors have made their mark in the sport in recent years. The female MMA scene is more dynamic and fascinating than ever as we enter 2023. We will talk about the fin",
+    image: "https://sportsurge.club/_next/image?url=https%3A%2F%2Fscdn.dev%2Fimages%2Fblog%2F10-top-female-mma-fighters-in-2023.webp&w=640&q=75"
+  },
+  {
+    url: "#",
+    title: '10 Top Female MMA Fighters in 2023',
+    excerpt: "Mixed Martial Arts (MMA) has gone a long way since its start, and female competitors have made their mark in the sport in recent years. The female MMA scene is more dynamic and fascinating than ever as we enter 2023. We will talk about the fin",
+    image: "https://sportsurge.club/_next/image?url=https%3A%2F%2Fscdn.dev%2Fimages%2Fblog%2F10-top-female-mma-fighters-in-2023.webp&w=640&q=75"
+  },
+  {
+    url: "#",
+    title: '10 Top Female MMA Fighters in 2023',
+    excerpt: "Mixed Martial Arts (MMA) has gone a long way since its start, and female competitors have made their mark in the sport in recent years. The female MMA scene is more dynamic and fascinating than ever as we enter 2023. We will talk about the fin",
+    image: "https://sportsurge.club/_next/image?url=https%3A%2F%2Fscdn.dev%2Fimages%2Fblog%2F10-top-female-mma-fighters-in-2023.webp&w=640&q=75"
+  }
+
+]
+
 export default function Home() {
   return (
     <main className="overflow-hidden">
@@ -130,29 +152,29 @@ export default function Home() {
                 WATCH
                 </Col>
               </Row>
-
+            </div>
               {
                 pop_events.map(event=>(
                   <div>
                       {
                           event.type == 'formula-1'
                           ? <a href="#" title={event.title} className="row MaclariListele align-items-center align-content-center">
-                            <Col xs={4} className='d-flex align-items-center'>
+                            <Col xs={5} className='d-flex align-items-center'>
                             <Image src="icon-f1.png" alt="formula-1-icon" className="responsive-img"/>
                             <div style={{color : '#fff', fontWeight : 'bold', marginLeft : '5px'}}>{event.title}</div>
                             </Col>
-                            <Col xs={3} className='league-logo'>
+                            <Col xs={2} className='league-logo'>
                               <Image src="icon-f1.png" alt="formula-1-icon" className="responsive-img"/>
                             </Col>
-                            <Col xs={3}>
+                            <Col xs={2}>
                             {event.time}
                             </Col>
-                            <Col xs={1}>
+                            <Col xs={2}>
                             <Image src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAAB/ElEQVRIie2VsWsUQRjF37d3np3FIQiCR25nj7vS4mIhiIgWaUxxSWUEsRELFQRrBUX/ApsUgkKCkQSLNFqoIIKg3n9wO7tgERDCgYiI7N08m1sYbG5ml02VV833mO/9+GaGXeBQByRx2aS1/gDgwqz8mmXZpV6v96sMOHDcd8xan6nX6xskXXuLg4MgWAWwn9cisqy1flgG7HTUAJAkyTmS7wA0ZhYBrCmlXhYBOx9XGIafSN6zLAHwTGu9WATsPHGuJEnWSd7Ia5J7jUZjsdVq7fnkeD+Q8Xh8i+THvBaRk1mWbY9Go6OVgvv9fjadTlcBpJZ9tlarrVcKBoBut7sfBMEAwO/cI3ktjuPbrhned2xLaz0AsGPlTElejqLozbzeUh8BpdRrkk8sqyYiG3EcR5WCZ/D7JHctqykim5WDi6o0WGv9SESWLWtMcm1eX9nHtQJg28qZAFhSSr2f11t44jRNTwN4YUEhInddoEDBiZMkOUHyG4BTlv1cKXXdNcN74uFweMQY8+o/6GdjzE2fHG9ws9l8KiLnLeu7iAw6nc7fysBxHN+x/0wA/gBYCcPwh08O4HHHWuuLAN4CqM8skrwSRdGWLxRwnDhN0wUAWxYUIvK4KNQZbIzZAXA8r0nuttvtB0WhzmAAP631l8lkclVETBnwoQ5M/wBTeLAZ7QRNBwAAAABJRU5ErkJggg==" alt="watch" className="responsive-img"/>
                             </Col>
                           </a>
                           :  <a href="#" title={event.title} className="row MaclariListele align-items-center align-content-center">
-                              <Col xs={4}>
+                              <Col xs={5}>
                                 <div className='d-flex align-items-center'>
                                   <Image src="427.png" alt="Guadalajara" className="responsive-img"/>
                                   <div style={{color : '#fff', fontWeight : 'bold', marginLeft : '5px'}}>{event.team_a}</div>
@@ -163,23 +185,40 @@ export default function Home() {
                                   <div style={{color : '#fff', fontWeight : 'bold', marginLeft : '5px'}}>{event.team_b}</div>
                                 </div>
                             </Col>
-                            <Col xs={3} className='league-logo'>
+                            <Col xs={2} className='league-logo'>
                               <Image src="league_placeholder.png" alt="league cup" className="responsive-img"/>
                             </Col>
-                            <Col xs={3}>
+                            <Col xs={2}>
                             <span className="live-btn">LIVE</span>
                             </Col>
-                            <Col xs={1}>
-                            <div className="btn btn-success btn-sm MobildeGizle">Watch</div>
+                            <Col xs={2}>
+                            <div className="btn btn-success btn-sm">Watch</div>
                             </Col>
                           </a>
                       }
                   </div>
                 ))
               }
-            </div>
           </div>
         </Col>
+        <br/>
+        <Row>
+        {
+          sport_blogs.map(blog=>(
+              <Col md={4} xs={12}>
+                  <a className="card bg-dark blog-item">
+                    <div>
+                      <Image src="blog1.png" className="card-img-top"/>
+                    </div>
+                    <div className="card-body">
+                      <h4 className="card-title">{blog.title}</h4>
+                      <p className="card-text">{blog.excerpt}</p>
+                    </div>
+                  </a>
+              </Col>
+          ))
+        }
+        </Row>
       </Container>
 
     </main>
